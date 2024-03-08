d25d12088f35be344f2cede2ea74aa748c6b79d5
Soumil-Biswas-Web <soumil.biswas19@hotmail.com>
2024-03-08 22:26:35 +0530

    finished work on index.html and javascript.js

--- javascript.js
+++ javascript.js
@@ -60,10 +60,84 @@ function playGame(){
     }
 }
 
+function createDiv(){
+    const body = document.querySelector('body');
+    score = document.createElement('div');
+    score.setAttribute('id', 'score');
+    const scoreDiv = document.createElement ('div');
+
+    const h11 = document.createElement ('h1');
+    h11.textContent = "Player Score";
+    scoreDiv.appendChild(h11);
+    const h12 = document.createElement ('h1');
+    h12.textContent = "CPU Score";
+    scoreDiv.appendChild(h12);
+
+    const pointDiv = document.createElement('div');    
+    h13 = document.createElement ('h1');
+    pointDiv.appendChild(h13);
+    h14 = document.createElement ('h1');
+    pointDiv.appendChild(h14);
+
+    score.appendChild(scoreDiv);    
+    score.appendChild(pointDiv);
+    body.appendChild(score);
+}
+
+function updateDiv(playerScore, cpuScore){
+    h13.textContent = playerScore;
+    h14.textContent = cpuScore;
+}
+
+function finishGame(){
+    const body = document.querySelector('body');
+    const resultText = document.createElement('h2');
+    resultText.setAttribute('style', 'text-align: center')
+    if (player == 5){
+        resultText.textContent = "Congratulations! You Won!"
+    }
+    else{
+        resultText.textContent = "死ね！ 雑魚！"
+    }
+    body.insertBefore(resultText, score);
+}
+
+function updateScore(){
+    if (score == undefined){
+        createDiv();
+        updateDiv(player, cpu);
+    }
+    else {
+        updateDiv(player, cpu);
+    }
+    if (player == 5 || cpu == 5){
+        finishGame();
+    }
+}
+
+function rpsOnClickRound(playerSelection){
+    if (player < 5 && cpu < 5){
+        const computerSelection = getComputerChoice();
+        result = playRound(playerSelection, computerSelection);
+        console.log(result);  
+        updateScore();     
+    }
+}
+
 /*const playerSelection = "Rock";
 const computerSelection = getComputerChoice();
 const result = playRound(playerSelection, computerSelection);
 console.log(result);*/
 var player = 0;
 var cpu = 0;
-playGame();
+var score, h13, h14;
+let result;
+
+const rock = document.querySelector('#rock');
+rock.onclick = () => rpsOnClickRound("rock");
+
+const paper = document.querySelector('#paper');
+paper.onclick = () => rpsOnClickRound("paper");
+
+const scissors = document.querySelector('#scissors');
+scissors.onclick = () => rpsOnClickRound("scissors");
